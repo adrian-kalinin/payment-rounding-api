b564de2cb4f7ee2dc4126234cfbad4994c95568e
Adrian Kalinin <adriankalinin@protonmail.com>
2021-08-02 19:56:01 +0300

    payment rounding endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from .views import PaymentRoundingView
+
+
+app_name = 'api'
 
 urlpatterns = [
-
+    path('', PaymentRoundingView.as_view(), name='payment-rounding')
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from decimal import Decimal
+from math import floor
+
+
+class PaymentRoundingView(APIView):
+    def post(self, request):
+        # Separate categories and count their prices
+
+        categories = {}
+
+        for invoice in request.data['invoice_lines']:
+            price = Decimal(invoice['unit_price_net']) * invoice['quantity']
+
+            if invoice['category'] in categories:
+                categories[invoice['category']] += price
+
+            else:
+                categories[invoice['category']] = price
+
+        # Generate bookkeeping data
+
+        payment_data = []
+        total_price = Decimal(sum(categories.values()))
+
+        for count, payment in enumerate(request.data['payments']):
+            data = {'id': count + 1, 'categorisations': []}
+
+            for category in categories:
+                net_amount = Decimal(payment['amount']) / total_price * categories[category]
+                floored = floor(net_amount * 100) / 100.0
+
+                data['categorisations'].append({
+                    'category': category,
+                    'net_amount': f'{floored:.2f}'
+                })
+
+            payment_data.append(data)
+
+        return Response(payment_data)
