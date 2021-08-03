e81617384c83834a3e02d40d616ccdc3e2022325
Adrian Kalinin <adriankalinin@protonmail.com>
2021-08-03 22:30:43 +0300

    fix generating bookkeeping data

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from decimal import Decimal
-from math import floor
 
 
 class PaymentRoundingView(APIView):
@@ -12,7 +11,7 @@ class PaymentRoundingView(APIView):
         categories = {}
 
         for invoice in request.data['invoice_lines']:
-            price = Decimal(invoice['unit_price_net']) * invoice['quantity']
+            price = int(invoice['unit_price_net'].replace('.', '')) * invoice['quantity']
 
             if invoice['category'] in categories:
                 categories[invoice['category']] += price
@@ -23,20 +22,29 @@ class PaymentRoundingView(APIView):
         # Generate bookkeeping data
 
         payment_data = []
-        total_price = Decimal(sum(categories.values()))
 
         for count, payment in enumerate(request.data['payments']):
             data = {'id': count + 1, 'categorisations': []}
 
-            for category in categories:
-                net_amount = Decimal(payment['amount']) / total_price * categories[category]
-                floored = floor(net_amount * 100) / 100.0
+            total_price = sum(categories.values())
+            available = int(payment['amount'].replace('.', ''))
 
-                data['categorisations'].append({
-                    'category': category,
-                    'net_amount': f'{floored:.2f}'
-                })
+            for price in categories.values():
+                distributed_amount = round(price / total_price * available)
+
+                if distributed_amount < price:
+                    amount_euros = f'{distributed_amount / 100:.2f}'
+                    available -= distributed_amount
+
+                else:
+                    amount_euros = f'{price / 100:.2f}'
+                    available -= price
+
+                data['categorisations'].append(amount_euros)
+                total_price -= price
 
             payment_data.append(data)
+
+        # Send response
 
         return Response(payment_data)
